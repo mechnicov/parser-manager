74b490f82a5eefd7481708e643c445c049cd7e8e
Edem Topuzov <keltikan@gmail.com>
2021-09-14 15:38:14 +0300

    Add URL validator

---
+++ app/lib/url_validator.rb
@@ -0,0 +1,16 @@
+require 'active_model'
+
+class UrlValidator < ActiveModel::EachValidator
+  def self.compliant?(value)
+    uri = URI::DEFAULT_PARSER.parse(value)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def validate_each(record, attribute, value)
+    unless value.present? && self.class.compliant?(value)
+      record.errors.add(attribute, 'is not a valid HTTP URL')
+    end
+  end
+end
