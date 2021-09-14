76698cf8d876f24394c64eb7fccb58fe6d975f74
Edem Topuzov <keltikan@gmail.com>
2021-09-14 15:44:14 +0300

    Add parser

---
+++ app/lib/parser.rb
@@ -0,0 +1,50 @@
+require 'pdf-reader'
+require 'docx'
+require 'msworddoc-extractor'
+
+class Parser
+  class << self
+    def call(file:, content:, file_type:)
+      case file_type
+      when :pdf then new(file, content).from_pdf
+      when :doc then new(file, content).from_doc
+      when :docx then new(file, content).from_docx
+      when :html then new(file, content).from_html
+      end
+    end
+  end
+
+  def initialize(file, content)
+    @file = file
+    @content = content
+  end
+
+  def from_pdf
+    PDF::Reader.new(file).pages.map(&:text).join("\n").strip
+  end
+
+  def from_doc
+    doc = MSWordDoc::Essence.new
+    ole = Ole::Storage.open(file)
+    doc.load_storage(ole)
+    doc.whole_contents.strip
+  end
+
+  def from_docx
+    Docx::Document.open(content).text
+  rescue Errno::ENOENT
+    raise ParseManager::UnsupportedFileTypeError
+  end
+
+  def from_html
+    html = Nokogiri::HTML(content)
+    html.css('script').remove
+    html.css('footer').remove
+    html.css('[class*="footer"]').remove
+    html.css('body').text.strip
+  end
+
+  private
+
+  attr_reader :file, :content
+end
