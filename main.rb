1479fa777ce3761bda8229c7ed600dafea4ee6a9
Edem Topuzov <keltikan@gmail.com>
2021-09-14 15:47:03 +0300

    Integrate API to application

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 require 'grape'
 
+Dir[File.join(__dir__, 'app', 'lib', '**', '*.rb')].each { |f| require f }
+Dir[File.join(__dir__, 'app', 'models', '**', '*.rb')].each { |f| require f }
+Dir[File.join(__dir__, 'app', 'api', '**', '*.rb')].each { |f| require f }
+
+DatabaseConnector.establish_connection
+
 module API
   class Root < Grape::API
     format :json
     prefix :api
+
+    mount V1::Parse
   end
 end
 
