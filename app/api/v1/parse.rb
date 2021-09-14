c5c3ea72c9363c91b65cc695e3920e1563bf7c0b
Edem Topuzov <keltikan@gmail.com>
2021-09-14 15:46:30 +0300

    Add API for parsing

---
+++ app/api/v1/parse.rb
@@ -0,0 +1,27 @@
+module API
+  module V1
+    class Parse < Grape::API
+      version :v1, using: :path
+
+      helpers do
+        def server_error!(message, status)
+          error!({ error: message }, status)
+        end
+      end
+
+      rescue_from :all, ->(e) { server_error!(e.message, 500) }
+      rescue_from ParseManager::UnsupportedFileTypeError, ->(e) { server_error!(e.message, 422) }
+      rescue_from :grape_exceptions
+
+      desc 'Parse the file'
+      params do
+        requires :url, type: String
+        requires :file, type: File
+      end
+      post :parse do
+        ParseManager.(file: params.dig(:file, :tempfile), url: params[:url])
+        { message: "#{params[:url]} was parsed successfully" }
+      end
+    end
+  end
+end
