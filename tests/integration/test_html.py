18683e160b05c2ce2ae6ad39b43496b82e91921d
Nikolay Lyudkevich <nikolai.lyudkevich@gmail.com>
2021-09-27 19:46:24 +0300

    Fix tests after dockerize

--- tests/integration/test_html.py
+++ tests/integration/test_html.py
@@ -1,19 +1,10 @@
-"""
-- сгенерировать данные;
-- положить их в базу данных;
-- отправить REST-запросы;
-- сверить результаты с ожидаемыми;
-- сгенерировать отчет по результатам.
-
-"""
-
+import configparser
 import logging
 import os
+import unittest
+
 import psycopg2
 import requests
-import unittest
-from requests.api import request
-import yaml
 
 
 URL = "http://127.0.0.1:9292/api/v1/parse"
@@ -21,22 +12,21 @@ URL = "http://127.0.0.1:9292/api/v1/parse"
 
 def connect_db():
     config_path = os.path.join(
-        os.path.dirname(__file__), '..', '..', 'config', 'database.yml')
+        os.path.dirname(__file__), '..', '..', '.env')
     with open(config_path) as f:
-        db_config = yaml.safe_load(f)
+        s = '[db_config]\n' + f.read()
+    config = configparser.ConfigParser()
+    config.read_string(s)
 
-    try: 
-        conn = psycopg2.connect(
-            user=db_config['user'],
-            # пароль, который указали при установке PostgreSQL
-            password=db_config['password'],
-            host="127.0.0.1",
-            port="5432",
-            database=db_config['database']
-        )
-    except: 
-        logging.error("Unable to connect to the database.") 
-        return None
+    db_config = config['db_config']
+
+    conn = psycopg2.connect(
+        user=db_config['POSTGRES_USER'],
+        password=db_config['POSTGRES_PASSWORD'],
+        host='0.0.0.0',
+        port='5555',
+        database=db_config['POSTGRES_DB']
+    )
 
     cur = conn.cursor() 
     return cur
--- tests/integration/test_pdf.py
+++ tests/integration/test_pdf.py
@@ -1,19 +1,10 @@
-"""
-- сгенерировать данные;
-- положить их в базу данных;
-- отправить REST-запросы;
-- сверить результаты с ожидаемыми;
-- сгенерировать отчет по результатам.
-
-"""
-
-import logging
+import configparser
 import os
+import re
+import unittest
+
 import psycopg2
 import requests
-import unittest
-from requests.api import request
-import yaml
 
 
 URL = "http://127.0.0.1:9292/api/v1/parse"
@@ -21,22 +12,21 @@ URL = "http://127.0.0.1:9292/api/v1/parse"
 
 def connect_db():
     config_path = os.path.join(
-        os.path.dirname(__file__), '..', '..', 'config', 'database.yml')
+        os.path.dirname(__file__), '..', '..', '.env')
     with open(config_path) as f:
-        db_config = yaml.safe_load(f)
+        s = '[db_config]\n' + f.read()
+    config = configparser.ConfigParser()
+    config.read_string(s)
 
-    try: 
-        conn = psycopg2.connect(
-            user=db_config['user'],
-            # пароль, который указали при установке PostgreSQL
-            password=db_config['password'],
-            host="127.0.0.1",
-            port="5432",
-            database=db_config['database']
-        )
-    except: 
-        logging.error("Unable to connect to the database.") 
-        return None
+    db_config = config['db_config']
+
+    conn = psycopg2.connect(
+        user=db_config['POSTGRES_USER'],
+        password=db_config['POSTGRES_PASSWORD'],
+        host='0.0.0.0',
+        port='5555',
+        database=db_config['POSTGRES_DB']
+    )
 
     cur = conn.cursor() 
     return cur
@@ -67,7 +57,11 @@ class TestPDF(unittest.TestCase):
         self.cursor.execute(
             f"SELECT parsed_data, file_type FROM pages WHERE url = '{data['url']}'")
         parsed_data, file_type = self.cursor.fetchone()
-        self.assertEqual(parsed_data, 'Привет, как дела\nВсё хорошо!\n\nСупер')
+        self.assertIsNotNone(
+            re.fullmatch(
+                r'Привет, как дела\n+Всё хорошо!\n+Супер', parsed_data
+            )
+        )
         self.assertEqual(file_type, 'pdf')
     
     def test_valid_file_with_invalid_ext(self):
@@ -90,7 +84,11 @@ class TestPDF(unittest.TestCase):
         self.cursor.execute(
             f"SELECT parsed_data, file_type FROM pages WHERE url = '{data['url']}'")
         parsed_data, file_type = self.cursor.fetchone()
-        self.assertEqual(parsed_data, 'Привет, как дела\nВсё хорошо!\n\nСупер')
+        self.assertIsNotNone(
+            re.fullmatch(
+                r'Привет, как дела\n+Всё хорошо!\n+Супер', parsed_data
+            )
+        )
         self.assertEqual(file_type, 'pdf')
     
     def test_no_url(self):
