2a4ed229b1a91d0f9d3abde7ce6df5a462c8cf91
Edem Topuzov <keltikan@gmail.com>
2021-09-27 22:35:20 +0300

    Monkey patching docx missed tab bug

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require 'dotenv/load'
 Dir[File.join(__dir__, '..', 'app', 'lib', '**', '*.rb')].each { |f| require f }
 Dir[File.join(__dir__, '..', 'app', 'models', '**', '*.rb')].each { |f| require f }
 Dir[File.join(__dir__, '..', 'app', 'api', '**', '*.rb')].each { |f| require f }
+Dir[File.join(__dir__, 'initializers', '**', '*.rb')].each { |f| require f }
 
 DatabaseConnector.establish_connection
 
