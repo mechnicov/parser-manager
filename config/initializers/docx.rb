68c783280e2500a178d5e62759cdaa040d6d3c98
Edem Topuzov <keltikan@gmail.com>
2021-09-29 00:54:40 +0300

    Precision fix missed tabs in docx more

--- config/initializers/docx.rb
+++ config/initializers/docx.rb
@@ -1,24 +1,11 @@
-module Docx
-  class Document
-    def initialize(path_or_io, options = {})
-      @replace = {}
+# Fix docx missed tabs bug
 
-      # if path-or_io is string && does not contain a null byte
-      if (path_or_io.instance_of?(String) && !/\u0000/.match?(path_or_io))
-        @zip = Zip::File.open(path_or_io)
-      else
-        @zip = Zip::File.open_buffer(path_or_io)
-      end
+require 'zip'
 
-      document = @zip.glob('word/document*.xml').first
-      raise Errno::ENOENT if document.nil?
-
-      @document_xml = document.get_input_stream.read.gsub("<w:tab/>", "<w:t>\t</w:t>")
-      @doc = Nokogiri::XML(@document_xml)
-      load_styles
-      yield(self) if block_given?
-    ensure
-      @zip.close
+module Zip
+  class InputStream
+    def sysread(length = nil, outbuf = '')
+      @decompressor.read(length, outbuf).gsub('<w:tab/>', "<w:t>\t</w:t>")
     end
   end
 end
